ee39afdb22a8fce90b6b7c73500e39be80625231
pumaraymond <91251213+pumaraymond@users.noreply.github.com>
2021-12-18 16:33:26 +0800

    Add files via upload

---
+++ C_MM15.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    while(scanf("%d %d",&x,&y)!=EOF){
+        if(x<=100&&y<=100&&x>=0&&y>=0)
+        printf("inside\n");
+        else printf("outside\n");
+    }
+    return 0; 
+}
